3ce43e557e02352822fcf14318f1c04c039b7947
Barkaviking <gorkemsengul@live.ca>
2025-07-06 19:34:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 import streamlit as st
 import pandas as pd
-import requests from bs4 
+import requests 
+from bs4 
 import BeautifulSoup
 
 # —————————————————————————————————————————————————————————————
